89e3077ba5b4267319d5c1db319300eec8cda2b6
Natnael Badigo <natibadigojimma@gmail.com>
2024-07-08 08:00:39 -0800

    1- auth controllers added.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
-import { connect } from "mongoose";
-import connectMongoDB from "../db/connectMongoDB.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse from data.
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 

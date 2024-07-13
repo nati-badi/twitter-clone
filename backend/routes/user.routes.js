d416e0b041d19e825d40c263725d6a7de5f0cbf4
Natnael Badigo <natibadigojimma@gmail.com>
2024-07-13 05:36:36 -0800

    1- user routes and controllers are added.

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from "../controllers/user.controller.js";
+// 
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.put("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/update", protectRoute, updateUser);
+
+export default router;

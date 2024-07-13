b4473f1e8c58b93bef12dc82f6b437725db13e77
Natnael Badigo <natibadigojimma@gmail.com>
2024-07-13 11:19:36 -0800

    1- post routes and controllers added.

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+
+export default router;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,7 +6,7 @@ import {
   getUserProfile,
   updateUser,
 } from "../controllers/user.controller.js";
-// 
+
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfile);
